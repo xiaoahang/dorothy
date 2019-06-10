d0fa6192c9bb341ad4f930b63c965c2aadad213b
hannah <hannah.zz@qq.com>
2019-06-10 23:10:06 +0800

    base dir join

---
+++ ch02/2-4/ner.py
@@ -0,0 +1,20 @@
+# -*-encoding=utf8-*-
+from stanfordcorenlp import StanfordCoreNLP
+import os
+
+project_base_dir = '/Users/weihangzhang/code/dl-nlp'
+
+nlp = StanfordCoreNLP(r'stanfordnlp', lang='zh')
+
+fin = open(os.path.join(project_base_dir, './ch02/2-4/news.txt'), 'r', encoding='utf8')
+fner = open(os.path.join(project_base_dir, './ch02/2-4/ner.txt'), 'w', encoding='utf8')  # 实体
+ftag = open(os.path.join(project_base_dir, './ch02/2-4/pos_tag.txt'), 'w', encoding='utf8')  # 词性
+for line in fin:
+    line = line.strip()
+    if len(line) < 1:  # 空行
+        continue
+
+    fner.write(" ".join([each[0] + "/" + each[1] for each in nlp.ner(line) if len(each) == 2]) + "\n")
+    ftag.write(" ".join([each[0] + "/" + each[1] for each in nlp.pos_tag(line) if len(each) == 2]) + "\n")
+fner.close()
+ftag.close()
